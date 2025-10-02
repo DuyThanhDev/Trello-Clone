b652be2835fb2ac8d7c9002d5df8283ee23f5a58
Apes <21040431.thanh@student.iuh.edu.vn>
2025-10-02 09:02:13 +0700

    Add Sidebar component and Separator utility; update package dependencies

---
+++ trello-tutorial/app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import { useLocalStorage } from "usehooks-ts";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Accordion } from "@radix-ui/react-accordion";
+
+
+interface SidebarProps {
+    storageKey?: string;
+};
+
+export const Sidebar = ({ 
+    storageKey = "t-sidebar-state",
+}: SidebarProps) => {
+
+    const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+        storageKey, 
+        {}
+    );
+
+    const { organization: activeOrganization,
+        isLoaded: isLoadingOrg
+     } = useOrganization();
+
+    const {
+        userMemberships,
+        isLoaded: isLoadingOrgList
+        } = useOrganizationList({
+            userMemberships: {
+                infinite: true,
+            },
+        });
+
+    const defaultAccordionValue: string[] = Object.keys(expanded)
+        .reduce((acc: string[], key: string) => {
+            if (expanded[key]) {
+                acc.push(key);
+            }
+            return acc;
+    }, []);
+
+    const onExpand = (id: string) => {
+        setExpanded((curr) => ({
+            ...curr,
+            [id]: !expanded[id],
+        }));
+    };
+
+    if (!isLoadingOrg || !isLoadingOrgList || userMemberships.isLoading) {
+        return (
+            <>
+                <Skeleton />
+            </>
+        );
+    }
+
+    return (
+        <>
+            <div className="font-medium text-xs flex items-center mb-1">
+                <span className="pl-4">
+                    Workspaces
+                </span>
+                <Button
+                    asChild
+                    type="button"
+                    size="icon"
+                    variant="ghost"
+                    className="ml-auto"
+                >
+                    <Link href="/select-org">
+                        <Plus 
+                            className="h-4 w-4"
+                        />
+                    </Link>
+                </Button>
+            </div>
+            <Accordion
+                type="multiple"
+                defaultValue={defaultAccordionValue}
+                className="space-y-2"
+            >
+                {userMemberships.data.map(({ organization }) => (
+                    <p key={organization.id}>
+                        {organization.id}
+                    </p>
+                ))}
+            </Accordion>
+        </>
+    );
+};
