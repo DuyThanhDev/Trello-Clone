9301ca06735de2938b46f4ad6118492f369996ec
Apes <21040431.thanh@student.iuh.edu.vn>
2025-10-16 22:26:05 +0700

    Implement board creation and deletion functionality; enhance form components and validation

--- trello-tutorial/actions/create-board.ts
+++ trello-tutorial/actions/create-board.ts
@@ -1,23 +1,50 @@
 "use server";
 
 import { z } from "zod";
-
 import { db } from "@/lib/db";
-import { title } from "process";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export type State = {
+    errors?: {
+        title?: string[];
+    };
+    message?: string | null;
+}
 
 const CreateBoard = z.object({
-    title: z.string(),
+    title: z.string().min(3, {
+        message: "Minimum length of 3 characters is required"
+    })
 });
 
-export async function create(formData: FormData) {
+export async function create(prevState: State, formData: FormData) {
 
-    const { title } = CreateBoard.parse({
+    const validateField = CreateBoard.safeParse({
         title: formData.get("title"),
     });
 
+    if (!validateField.success) {
+        return {
+            errors: validateField.error.flatten().fieldErrors,
+            message: null
+        }
+    }
+
+    const { title } = validateField.data;
+
+    try {
         await db.board.create({
             data: {
                 title,
             }
-    });
+        });
+    } catch (error) {
+        return {
+            message: "Database error.",
+        };
+    }
+
+    revalidatePath("/organization/org_33UOIY4WOGOjPX4U43duveUShZs");
+    redirect("/organization/org_33UOIY4WOGOjPX4U43duveUShZs");
 }
